b5291039f9c3adffe149a78b5a6893d8f0b65278
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-14 19:36:24 -0700

    Tinker with dirver orders

--- project_2_front/src/app/components/driver-orders/driver-orders.component.ts
+++ project_2_front/src/app/components/driver-orders/driver-orders.component.ts
@@ -6,35 +6,51 @@ import { OrdersService } from 'src/app/services/orders.service';
 @Component({
   selector: 'app-driver-orders',
   templateUrl: './driver-orders.component.html',
-  styleUrls: ['./driver-orders.component.css']
+  styleUrls: ['./driver-orders.component.css'],
 })
 export class DriverOrdersComponent implements OnInit {
   selectedOrder?: Order;
 
   ngOnInit(): void {
-      this.getOrders();
+    this.getOrders();
   }
 
-  public readyOrders : Order[] = [];
-  constructor(private os : OrdersService) { }
+  public activeOrder?: Order;
+
+  public readyOrders: Order[] = [];
+  public activeOrders: Order[] = [];
+
+  public hasActiveOrder = false;
+
+  constructor(private os: OrdersService) {}
 
   getOrders() {
     this.os.getOrdersByStatus(2).subscribe(
-      (data: any) => {
-        this.readyOrders = data.body;
-        console.log("data: " + data.body)
-          }
-        );
+      (data) => {
+        this.readyOrders = data.body as Order[];
+        console.log('data: ' + data.body);
+      },
+      (error) => {
+        console.log(error);
       }
-    
-      takeOrder() {
-    let orderStatus: OrderStatus;
+    );
 
+    this.os.getOrdersByStatus(3).subscribe(
+      (data) => {
+        this.activeOrders = data.body as Order[];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  takeOrder(order: Order) {
+    let orderStatus: OrderStatus;
+    this.activeOrder = order;
+    this.hasActiveOrder = true;
 
     // I want to change the order status of a specific order ID
     // order.orderId
   }
-
-  }
-
-  
+}
